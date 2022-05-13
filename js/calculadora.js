6accbbc5eb62f810ef6f803dbe28ec0fe0d8e541
Sofi <sofiapazrodriguez@gmail.com>
2022-05-12 22:40:07 -0300

    probando calculadorajs

---
+++ js/calculadora.js
@@ -0,0 +1,57 @@
+console.log('estamos en el archivo de la calculadora');
+
+//let numeroUno = Number(prompt('ingresa un numero para realizar tu calculo'));
+//let numeroDos = Number(prompt('ingresa otro numero para realizar tu calculo'));
+
+//console.log('el primer numero es:', numeroUno);
+//console.log('el segundo numero es:', numeroDos);
+// funcion escribir: le paso un parametro a la funcion, viaja hasta aqui e imprime esto
+
+// como traer datos de los inputs a traves de variables: javascript accede a las etiquetas del html a traves de:
+
+
+
+//eventos: click en boton, que guarde el valor de las variables y que haga la operacion correspondiente
+
+/*de la clase12
+function escribir (r) {
+    console.log('el resultado es:', r);
+    
+}
+
+function Sumar(a, b){
+    let resultado = a +b;
+    escribir(resultado);
+}
+
+function Resta(a, b){
+    let resultado = a-b;
+
+}
+
+//Sumar(numeroUno, numeroDos);
+
+//con ejemplo del compañero con if y 3 parametros del
+
+//NaN en javascript significa not a number
+
+function calcular (a, b, c) {
+    if (condition) {
+        
+    }
+} */ 
+
+/*function calcular (){
+
+    let numuno = document.getElementById('numuno').value;
+    let numdos = document.getElementById('numdos').value;
+    
+    console.log(numuno);
+    console.log(numdos);
+
+} */
+
+//scope:alcance de las variables. puedo llevar let numuno y let numdos como variables globales y escribir menos codigo
+
+//me quedo en el 1:20 hs
+
